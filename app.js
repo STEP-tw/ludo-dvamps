6ae337b4b4568b2a2c3c8aa75f50e86279d3e63e
Ashish Kumar Joy <ashishkujoy@gmail.com>
2018-02-14 15:47:53 +0530

    common development environment has been added

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(express.static('public'));
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-const PORT = 9000;
+const http = require('http');
+const app = require('./app.js');
+const PORT = 8000;
+const server = http.createServer(app);
+server.listen(PORT);
+console.log(`server listening at ${PORT}`);
