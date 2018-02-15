a2392dfa8ffc8c7c7b019bd6eee4b638dfaf64aa
dhanusha <dhanalakshmilanda@gmail.com>
2018-02-15 11:30:39 +0530

    What changes have you made?  * Added Games Manager.  * Added Game.
    Why do we need this change?
     * To create a new game.
    
    What can this change impact?
     * Given a name, a new game can be created.

---
+++ src/models/game.js
@@ -0,0 +1,7 @@
+class Game {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+module.exports = Game;
---
+++ test/unitTest/gameManagerTest.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const GamesManager = require(path.resolve('src/models/gamesManager.js'));
+const Game = require(path.resolve('src/models/game.js'));
+const assert = require('chai').assert;
+
+describe('#GamesManager', () => {
+  let gamesManager;
+  beforeEach(function() {
+    gamesManager = new GamesManager();
+  });
+
+  describe('#addGame', () => {
+    it('should create a new game with given name and store it', () => {
+      let game = gamesManager.addGame('newGame');
+      assert.deepEqual(game, new Game('newGame'));
+    });
+  });
+
+  describe('#doesGameExists', () => {
+    it('should return true if game of given name exists ', () => {
+      gamesManager.addGame('newGame');
+      assert.isOk(gamesManager.doesGameExists('newGame'));
+    });
+    it('should return false if game of given name does not exists ', () => {
+      assert.isNotOk(gamesManager.doesGameExists('newGame'));
+    });
+  });
+});
