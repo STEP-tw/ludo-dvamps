4cc1330d39359190d4dc988d396d3270ae14a7fb
manish yadav <manishyadav12354@gmail.com>
2018-02-15 19:24:32 +0530

    [#4] salman/manish - updated getStatus and created DeleteHandler
    What changes have you made?
    
    * updated getStatus function
    * created app DeleteHandler
    
    Why do we need this change?
    
    *
    
    What can this change impact?

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const rfs = require('rotating-file-stream');
 const GamesManager = require(path.resolve('src/models/gamesManager.js'));
-const postHandlers = require(path.resolve('src/handlers/postHandlers.js'));
-const defaultHandlers = require(path.resolve('src/handlers/defaultHandler.js'));
-const lib = require('./src/lib/utility.js');
 const getHandlers = require(path.resolve('src/handlers/getHandlers.js'));
+const postHandlers = require(path.resolve('src/handlers/postHandlers.js'));
+const deleteHandler = require(path.resolve('src/handlers/deleteHandler.js'));
+const defaultHandlers = require(path.resolve('src/handlers/defaultHandler.js'));
 const app = express();
 const gamesManager = new GamesManager();
 let logDir = path.resolve('logs/');
@@ -26,31 +26,22 @@ let accessLogStream = rfs(lognameGenerator, {
 app.initialize = function(gamesManager) {
   app.gamesManager = gamesManager;
 };
-app.use(logger('combined', {stream: accessLogStream}));
-app.use(express.urlencoded({extended:false}));
+app.use(logger('combined', {
+  stream: accessLogStream
+}));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(express.json());
 
-app.get('/',defaultHandlers.handleSlash);
-app.get('/getAvailableGames',getHandlers.serveAvailableGames);
+app.get('/', defaultHandlers.handleSlash);
+app.get('/getAvailableGames', getHandlers.serveAvailableGames);
 
 app.use(express.static('public'));
-app.post('/createGame',postHandlers.createNewGame);
 app.use(cookieParser());
-app.get('/gameName',(req,res)=>{
-  res.send(lib.toS(req.cookies.gameName));
-});
-app.get('/userName',(req,res)=>{
-  res.send(lib.toS(req.cookies.playerName));
-});
-app.delete('/player',(req,res)=>{
-  let gameName=req.body.gameName;
-  let player= req.body.playerName;
-  let game = app.gamesManager.getGame(gameName);
-  game.removePlayer(player);
-});
-app.post('/getStatus',(req,res)=>{
-  let gameName=req.body.gameName;
-  let game = app.gamesManager.getGame(gameName);
-  res.send(lib.toS(game.getStatus()));
-});
+app.post('/createGame', postHandlers.createNewGame);
+app.get('/gameName', getHandlers.serveGameName);
+app.get('/userName', getHandlers.serveUserName);
+app.delete('/player', deleteHandler.removePlayer);
+app.get('/getStatus', getHandlers.serveGameStatus);
 module.exports = app;
--- src/handlers/getHandlers.js
+++ src/handlers/getHandlers.js
@@ -1,8 +1,22 @@
+const lib = require('../lib/utility.js');
 const serveAvailableGames = function(req,res){
   let availableGames = req.app.gamesManager.getAvailableGames();
   res.send(availableGames);
 };
-
+const serveGameName=(req,res)=>{
+  res.send(req.cookies.gameName);
+};
+const serveUserName=(req,res)=>{
+  res.send(req.cookies.playerName);
+};
+const serveGameStatus=(req,res)=>{
+  let gameName=req.cookies.gameName;
+  let game = req.app.gamesManager.getGame(gameName);
+  res.send(lib.toS(game.getStatus()));
+};
 module.exports = {
-  serveAvailableGames
+  serveAvailableGames,
+  serveGameName,
+  serveUserName,
+  serveGameStatus
 };
