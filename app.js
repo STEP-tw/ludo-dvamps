7beb4a9705a39d8f872edc2a847073f73b2f8783
Debarun Chakraborty <debarun.chakraborty96@gmail.com>
2018-02-15 11:19:53 +0530

    [Debarun] Some changes in script and added morgan as a logger
    What changes have you made?
    
    * pre-commit hooks removed and pre-push hook added
    * added morgan logger
    
    Why do we need this change?
    
    * pre-commit hooks was making trouble in development
    * to track log files in day to day basis morgan is needed
    
    What can this change impact?
    
    * pre-push hooks will help to develop faster and also track no failing
    tests are pushed
    * log files in day to day basis will help to track logs faster

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const path = require('path');
+const rfs = require('rotating-file-stream');
 
+let logDir = path.resolve('logs/');
+
+let accessLogStream = rfs('access.log', {
+  interval: '1d',
+  path: logDir
+});
+
+app.use(morgan('combined', {stream: accessLogStream}));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(express.static('public'));
