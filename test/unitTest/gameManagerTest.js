5df3faad70ef4e8197495d4edd92cc6ae623625e
manish yadav <manishyadav12354@gmail.com>
2018-02-15 14:27:39 +0530

    [#4] salman/manish - solved merge conflict in gameManagerTest
    What changes have you made?
    
    *
    
    Why do we need this change?
    
    *
    
    What can this change impact?

--- test/unitTest/gameManagerTest.js
+++ test/unitTest/gameManagerTest.js
@@ -2,16 +2,18 @@ const assert = require('chai').assert;
 const GameManager = require('../../src/models/gameManager.js');
 let gameManager;
 describe('#GameManager', () => {
-  beforeEach(()=>{
-    gameManager=new GameManager();
+  beforeEach(() => {
+    gameManager = new GameManager();
   });
   describe('#getGame()', () => {
     it('should return Game', () => {
-      let expectedGame={
-        players:[],
-        status:{players:[]}
-      }
-      assert.deepEqual(gameManager.getGame('D-VAMPS'),expectedGame);
+      let expectedGame = {
+        players: [],
+        status: {
+          players: []
+        }
+      };
+      assert.deepEqual(gameManager.getGame('D-VAMPS'), expectedGame);
       assert.isUndefined(gameManager.getGame('SAMAJ'));
     });
   });
