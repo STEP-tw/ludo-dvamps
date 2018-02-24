710e7cbf185f5fd1dd0e62689514055e56c0a724
Ashish Kumar Joy <ashishkujoy@gmail.com>
2018-02-24 21:42:34 +0530

    [#7] Ashish - kill coin
    What changes have you made?
    
    * cell will now return object containing details, wether it has killed opponent coin
     if has cutted then whose coin.
    
    Why do we need this change?
    
    * To replace this status in place of event emitter to increment currentPlayer chance.
    
    What can this change impact?

--- src/models/coin.js
+++ src/models/coin.js
@@ -1,28 +1,4 @@
 let EventEmitter = require('events');
-// class Coin {
-//   constructor(id,homePosition,eventEmitter) {
-//     this.id = id;
-//     this.position = homePosition;
-//     this.homePosition = homePosition;
-//     this.color;
-//     this.eventEmitter = eventEmitter;
-//   }
-//   getPosition(){
-//     return this.position;
-//   }
-//   getStatus(){
-//     return {id:this.id,position:this.position,color:this.color};
-//   }
-//   setColor(color) {
-//     this.color = color;
-//   }
-//   setPosition(pos){
-//     this.position = pos;
-//   }
-//   die(){
-//     this.eventEmitter.emit('died',{id:this.id,color:this.color});
-//   }
-// }
 class Coin extends EventEmitter{
   constructor(id,homePosition) {
     super();
