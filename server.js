7d792d1d40c64d715443b2a16b2ff034d8f7816d
Ashish Kumar Joy <ashishkujoy@gmail.com>
2018-03-02 11:59:56 +0530

    [#48] Debu/Ashish - added timezone management
    What changes have you made?
    
    * Added moment js
    
    Why do we need this change?
    
    * To let user see the time in activity log as per their local time

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ const GamesManager = require(path.resolve('src/models/gamesManager.js'));
 const ColorDistributer = require('./src/models/colorDistributer.js');
 
 const PORT = process.env.PORT || 8000;
+const timeStamp = ()=>new Date();
 
-app.initialize(new GamesManager(ColorDistributer,new Dice(Math.random)));
+app.initialize(new GamesManager(ColorDistributer,
+  new Dice(Math.random),timeStamp)
+);
+
 const server = http.createServer(app);
 server.listen(PORT);
 console.log(`server listening at ${PORT}`);
