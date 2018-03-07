53b6872c1df6bb6d2e5c0fac30b2c1d0781cb4b3
Ashish Kumar Joy <ashishkujoy@gmail.com>
2018-03-07 20:24:32 +0530

    removed unnecessary file foo

--- foo.js
+++
@@ -1,8 +0,0 @@
-const Dice = require('./src/models/dice.js');
-const path = require('path');
-const GamesManager = require(path.resolve('src/models/gamesManager.js'));
-const ColorDistributer = require('./src/models/colorDistributer.js');
-const timeStamp = ()=>new Date();
-let gameMananger = new GamesManager(ColorDistributer,
-  new Dice(Math.random),timeStamp);
-module.exports = gameMananger;
