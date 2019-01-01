9a6372be071ecbaccfeb141582886eb5a4d7bf99
chakradeb <debarun.chakraborty96@gmail.com>
2019-01-01 20:55:43 +0530

    Separate dev and prod environment using env variables

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const path = require('path');
 const {Client} = require('pg');
 const GamesManager = require(path.resolve('src/models/gamesManager.js'));
 const SessionManager = require(path.resolve('src/models/sessionManager.js'));
-const ColorDistributer = require('./src/models/colorDistributer.js');
+const ColorDistributer = require(path.resolve('src/models/colorDistributer.js'));
+const randomiser = require(path.resolve('src/models/randomiser.js'));
+
+const version_name = process.env.APP_VERSION;
 
 const idGenerator = function(){
   return new Date().getTime();
@@ -19,8 +22,9 @@ const client = new Client(connectionString);
 client.connect();
 
 app.initialize(new GamesManager(
-  ColorDistributer,new Dice(Math.random),timeStamp),
+  ColorDistributer,new Dice(randomiser(version_name)),timeStamp),
 new SessionManager(idGenerator),client);
 const server = http.createServer(app);
 server.listen(PORT);
 console.log(`server listening at ${PORT}`);
+console.log(`=========== Running ${version_name} version ===========`);
